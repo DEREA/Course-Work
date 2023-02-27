a48259ed9d2ebd1b655176ff8b9586dfa0fb404d
DEREA <89138555+DEREA@users.noreply.github.com>
2023-02-27 12:02:16 +0200

    Add files via upload

---
+++ DelegatePractice/DelegatePractice/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DelegatePractice
+{
+    delegate void MyDelegate(string s, int i);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // MyDelegate messageDelegate =  = Console.WriteLine(name,12);
+            MyDelegate del = new MyDelegate(AMethod);
+
+            Console.WriteLine("Give a name");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Give a number");
+            int num = Convert.ToInt32( Console.ReadLine());
+            del(name,num);
+
+        }
+        static void AMethod(string name, int num)
+        {
+            Console.WriteLine("Name: "+name+" Num: "+num);
+        }
+    }
+   
+}
---
+++ DictionaryAndIPClass/DictionaryAndIPClass/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace DictionaryAndIPClass
+{
+    class Program
+    {
+        
+
+        public class IPAddresses : System.Collections.DictionaryBase
+        {
+            public IPAddresses()
+            {
+            }
+            public void Add(string name, string ip)
+            {
+                base.InnerHashtable.Add(name, ip);
+            }
+            public string Item(string name)
+            {
+                return base.InnerHashtable[name].ToString();
+            }
+            public void Remove(string name)
+            {
+                base.InnerHashtable.Remove(name);
+            }
+
+
+
+        }//end of IPAddresses
+
+
+
+
+
+        class YourClass
+        {
+
+
+
+
+            static void Main()
+            {
+
+
+
+                IPAddresses myIPs = new IPAddresses();
+                myIPs.Add("Mike", "192.155.12.1");
+                myIPs.Add("David", "192.155.12.2");
+                myIPs.Add("Bernica", "192.155.12.3");
+
+                Console.WriteLine("There are " + myIPs.Count + " IP addresses");
+                Console.WriteLine("David's ip address: " + myIPs.Item("David"));
+
+
+
+                var myList = new List<KeyValuePair<string, string>>();
+
+                // adding elements
+                myList.Add(new KeyValuePair<string, string>(myIPs.Item("Mike"), "192.155.12.1"));
+                myList.Add(new KeyValuePair<string, string>(myIPs.Item("David"), "192.155.12.2"));
+                myList.Add(new KeyValuePair<string, string>(myIPs.Item("Bernica"), "192.155.12.3"));
+
+
+
+                myIPs.Clear();
+
+                Console.WriteLine("Clearing");
+
+                Console.WriteLine("There are " + myIPs.Count + " IP addresses");
+
+
+
+            }//end of main method
+
+
+
+        }//end of YourClass
+    }
+}
