357e13fe598758d9dc8ecd862b34320a6574afde
DEREA <89138555+DEREA@users.noreply.github.com>
2023-02-27 12:15:07 +0200

    Add files via upload

---
+++ Test2Q4/Test2Q4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Test2Q4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> provinces = new List<string>();
+
+            provinces.Add("Western Cape");
+            provinces.Add("Eastern Cape");
+            provinces.Add("Northern Cape");
+            provinces.Add("North West");
+            provinces.Add("Free State");
+            provinces.Add("Kwazulu Natal");
+            provinces.Add("Gauteng");
+            provinces.Add("Limpopo");
+            provinces.Add("Mpumalanga");
+
+            provinces.Sort();
+           
+            provinces.ToArray();
+
+            for (int i=0;i< provinces.Count;i++) 
+            {
+                Console.WriteLine(provinces[i]);
+            }
+            
+
+
+
+        }
+    }
+}
