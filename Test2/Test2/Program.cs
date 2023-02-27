64ac638d72758a0e8ad6f3166e17538df28d560f
DEREA <89138555+DEREA@users.noreply.github.com>
2023-02-27 12:14:19 +0200

    Add files via upload

---
+++ Test2/Test2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+           // int a, b, c, d;
+
+           // Random rnd = new Random();
+
+           // a = rnd.Next(1, 10);
+           // b = rnd.Next(1, 10);
+           // c = rnd.Next(1, 10);
+           // d = rnd.Next(1, 10);
+
+           //int ans = a + b + c + d;
+
+            for (int a =0;a<10;a++) 
+            {
+                for (int b = 0; b < 10; b++)
+                {
+                    for (int c = 0; c < 10; c++)
+                    {
+                        for (int d = 0; d < 10; d++)
+                        {
+
+
+                            if ((a + b) / 2 > 2 &&(c + d) / 2 > 2)
+                            {
+
+                                Console.WriteLine((a+b)+""+(c+d));
+
+                            }
+                           // ans = ans + 1;
+                           // d = d + 1;
+
+                        }
+                       // c = c + 1;
+
+                    }
+                   // b = b + 1;
+
+                }
+              //  a = a + 1;
+
+            }
+
+
+        }
+    }
+}
---
+++ Test2Q2/Test2Q2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+namespace Test2Q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\k254l\source\repos\Test2Q2";
+
+            if (File.Exists(path)) 
+            {
+                File.Delete(path);
+            }
+            
+            Console.WriteLine("Enter a name");
+            string name = Console.ReadLine();
+
+            StreamWriter sw;
+            sw = File.CreateText(@"C:\Users\k254l\source\repos\Test2Q2\\MyFile.txt");
+
+
+
+            for (int i = 0; i< 5;i++) 
+            {
+                sw.Write(name+",");
+            }
+            
+            sw.Close();
+
+            string text = File.ReadAllText(path);
+            string phrase = text;
+
+            string[] words = phrase.Split(',');
+
+            for(int i = 0; i < words.Length; i++)
+            {
+                System.Console.WriteLine(words[i]);
+            }
+
+
+        }
+    }
+}
