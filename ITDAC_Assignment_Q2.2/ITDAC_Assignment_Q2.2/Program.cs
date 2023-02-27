e54ca39aa815c464319735795e0e725fd3779cad
DEREA <89138555+DEREA@users.noreply.github.com>
2023-02-27 12:03:58 +0200

    Add files via upload

---
+++ ITDAC_Assignment_Q2.2/ITDAC_Assignment_Q2.2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ITDAC_Assignment_Q2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Bubble sort \n \n");
+
+
+            int[] intArr = { 72,90,78,82,68,73,85,93,87,65 };
+
+
+            Console.WriteLine("Unsorted array: ");
+
+
+            for (int i = 0; i < intArr.Length; i++)
+            {
+                Console.Write(intArr[i] + ", ");
+            }
+
+
+            Console.WriteLine("\n \n Ascending");
+
+
+            int a;
+            int b;
+            int temp;
+
+            for (int j = 0; j < intArr.Length - 1; j++)
+            {
+
+                for (int i = 0; i < intArr.Length - 1; i++)
+                {
+                    a = intArr[i];
+
+                    b = intArr[i + 1];
+
+                    if (b < a)
+                    {
+                        temp = a;
+                        intArr[i] = b;
+                        intArr[i + 1] = temp;
+                    }
+
+                }
+            }
+
+            for (int i = 0; i < intArr.Length; i++)
+            {
+                Console.Write(intArr[i] + ", ");
+            }
+            Console.WriteLine("\n\n");
+        }
+    }
+}
---
+++ ITDCA_Assignment_Q2.4/ITDCA_Assignment_Q2.4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountRepeatedWordCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Word;
+            Console.WriteLine("Type the sentence below");
+            Word = Console.ReadLine();     
+            var Value = Word.Split(' ');   
+
+            Dictionary<string, int> RepeatedWordCount = new Dictionary<string, int>();
+
+            for (int i = 0; i < Value.Length; i++) 
+            {
+                if (RepeatedWordCount.ContainsKey(Value[i])) 
+                {
+                    int value = RepeatedWordCount[Value[i]];
+                    RepeatedWordCount[Value[i]] = value + 1;
+                }
+                else
+                {
+                    RepeatedWordCount.Add(Value[i], 1);   
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("___________________________________");
+            Console.WriteLine("Repeated words and counts:\n");
+            foreach (KeyValuePair<string, int> kvp in RepeatedWordCount.OrderBy(key => key.Key))
+            {
+                Console.WriteLine(kvp.Key + " Counts: " + kvp.Value);  
+            }
+
+                Console.ReadKey();
+
+            
+        }
+    }
+}
