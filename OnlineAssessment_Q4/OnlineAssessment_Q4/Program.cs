1f9c31059f963eba1c013fa991f85334df7989be
DEREA <89138555+DEREA@users.noreply.github.com>
2023-02-27 12:18:41 +0200

    Add files via upload

---
+++ OnlineAssessment_Q4/OnlineAssessment_Q4/Program.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineAssessment_Q4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 51, 31, 22, 17, 12, 9, 18, 0 };
+			MaxHeap heap1 = new MaxHeap();
+
+			
+			heap1.insert(51);
+			heap1.insert(31);
+			heap1.insert(22);
+			heap1.insert(17);
+			heap1.insert(12);
+			heap1.insert(9);
+			heap1.insert(18);
+			heap1.insert(0);
+
+
+
+			heap1.preorder(heap1.root);
+			
+			//4.2.
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("4.2.a Inserting 68");
+			Console.WriteLine("");
+
+			heap1.insert(68);
+
+			heap1.preorder(heap1.root);
+
+
+
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("4.2.b Array representation");
+			Console.WriteLine("");
+
+			arr[arr.Length - 1] = 68;
+
+			for (int i = 0;i<arr.Length;i++)
+			{
+				Console.Write(""+arr[i]+",");
+			}
+
+
+
+
+
+			/*
+			MaxHeap heap2 = new MaxHeap();
+			
+
+			for (int i = 15; i > 0; i--)
+			{
+				heap2.insert(i);
+			}
+			Console.WriteLine();
+			heap2.preorder(heap2.root);
+
+			Console.ReadLine();
+			*/
+			Node nde=heap1.root;
+			deleteRoot(arr,nde.key);
+
+
+
+
+			Console.ReadLine();
+
+			//4.4. The complexity is quite high
+
+
+
+		}
+
+
+
+		static void heapify(int[] arr, int n, int i)
+		{
+			int largest = i; // Initialize largest as root
+			int l = 2 * i + 1; // left = 2*i + 1
+			int r = 2 * i + 2; // right = 2*i + 2
+
+
+			if (l < n && arr[l] > arr[largest])
+				largest = l;
+
+
+			if (r < n && arr[r] > arr[largest])
+				largest = r;
+
+
+			if (largest != i)
+			{
+				int swap = arr[i];
+				arr[i] = arr[largest];
+				arr[largest] = swap;
+
+
+				heapify(arr, n, largest);
+			}
+		}
+
+
+		static int deleteRoot(int[] arr, int n)
+		{
+
+			int lastElement = arr[n - 1];
+
+
+			arr[0] = lastElement;
+
+
+			n = n - 1;
+
+
+			heapify(arr, n, 0);
+
+
+			return n;
+		}
+
+
+
+
+
+
+
+	}
+
+
+
+
+
+
+
+	public class Node
+	{
+		//Left and right child
+		public Node left;
+		public Node right;
+		//Data value
+		public int key;
+		public Node(int key)
+		{
+			this.key = key;
+			left = null;
+			right = null;
+		}
+	}
+	public class MaxHeap
+	{
+		//This is use to store information of number of nodes in Max heap
+		public int size;
+		public Node root;
+		public MaxHeap()
+		{
+			root = null;
+			size = 0;
+		}
+		//Get height of insert new node
+		public int insertHeight()
+		{
+			int i = 1;
+			int sum = 0;
+			while (this.size > sum + (1 << i))
+			{
+				sum += (1 << i);
+				i++;
+			}
+			return i;
+		}
+		public void swapNode(Node first, Node second)
+		{
+			int key = first.key;
+			first.key = second.key;
+			second.key = key;
+		}
+		//Arrange node key
+		public void arrangeNode(Node root)
+		{
+			if (root.left != null && root.left.key > root.key)
+			{
+				swapNode(root, root.left);
+			}
+			if (root.right != null && root.right.key > root.key)
+			{
+				swapNode(root, root.right);
+			}
+		}
+		public Boolean addNode(Node root, int height, int level, int key)
+		{
+			if (level >= height)
+			{
+				return false;
+			}
+			if (root != null)
+			{
+				if (level - 1 == height && root.left == null || root.right == null)
+				{
+					if (root.left == null)
+					{
+						root.left = new Node(key);
+					}
+					else
+					{
+						root.right = new Node(key);
+					}
+					arrangeNode(root);
+					return true;
+				}
+				if (addNode(root.left, height, level + 1, key) ||
+					addNode(root.right, height, level + 1, key))
+				{
+					arrangeNode(root);
+					return true;
+				}
+			}
+			return false;
+		}
+		//Handles the request to new inserting node
+		public void insert(int key)
+		{
+			//Test case
+
+			if (root == null)
+			{
+				root = new Node(key);
+			}
+			else
+			if (root.left == null)
+			{
+				root.left = new Node(key);
+				arrangeNode(root);
+			}
+			else
+			if (root.right == null)
+			{
+				root.right = new Node(key);
+				arrangeNode(root);
+			}
+			else
+			{
+				int height = insertHeight();
+				addNode(root, height, 0, key);
+			}
+			this.size++;
+		}
+		public void preorder(Node root)
+		{
+			if (root != null)
+			{
+				Console.Write(" " + root.key);
+				preorder(root.left);
+				preorder(root.right);
+			}
+		}
+
+	}
+
+
+
+}
+
---
+++ OnlineAssessment_Q5/OnlineAssessment_Q5/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+namespace OnlineAssessment_Q5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Hashtable numberNames = new Hashtable();
+            numberNames.Add("Bem", 12); 
+            numberNames.Add("Chloe", 5);
+            numberNames.Add("Jane", 23);
+            numberNames.Add("Den", 45);
+            numberNames.Add("Mike", 90);
+
+            foreach (DictionaryEntry de in numberNames)
+                Console.WriteLine("Key: {0}, Value: {1}", de.Key, de.Value);
+
+
+            string[] name =new string[] {"Bem","Chloe","Jane", "Den","Mike" };
+            int[] age =new int[] { 12,5,23,45,90};
+
+            string nm;
+            int ge;
+
+            for (int i=0;i<age.Length-1;i++)
+            {
+                nm = name[i];
+                ge = age[i];
+                Console.WriteLine(sumAscii(nm,ge));
+            }
+
+
+            try
+            {
+                numberNames.Add("12", "Bem");
+                numberNames.Add("5", "Chole");
+                numberNames.Add("23", "Jane");
+                numberNames.Add("45", "Den");
+                numberNames.Add("90", "Mike");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("This student is already in the list");
+            }
+
+
+            if (numberNames.ContainsValue("Nuha Ali"))
+            {
+                Console.WriteLine("This student name is already in the list");
+            }
+            else
+            {
+                Console.WriteLine("This student name isnt in the list");
+            }
+
+
+
+          
+            ICollection key = numberNames.Keys;
+
+            foreach (string k in key)
+            {
+                Console.WriteLine(k + ": " + numberNames[k]);
+            }
+            Console.ReadKey();
+
+            Console.ReadLine();
+
+        }
+
+
+        
+ 
+
+            // Function that returns true
+            // if n is prime
+            static bool isPrime(int n)
+            {
+                if (n == 0 || n == 1)
+                {
+                    return false;
+                }
+
+                for (int i = 2; i * i <= n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            // Function to return the sum
+            // of the ascii values of the characters
+            // which are present at prime positions
+           public static int sumAscii(string str, int n)
+            {
+                // To store the sum
+                int sum = 0;
+
+                // For every character
+                for (int i = 0; i < n; i++)
+                {
+
+                    // If current position is prime
+                    // then add the ASCII value of the
+                    // character at the current position
+                    if (isPrime(i + 1))
+                    {
+                        sum += (int)(str[i]);
+                    }
+                }
+
+                return sum;
+            }
+
+            // Driver code
+            public static void Main()
+            {
+                string str = "geeksforgeeks";
+                int n = str.Length;
+
+                Console.WriteLine(sumAscii(str, n));
+            }
+        }
+
+
+    }
+}
