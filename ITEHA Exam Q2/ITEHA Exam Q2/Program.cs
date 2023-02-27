741b52010b7d087d751715b83381925f26709bb0
DEREA <89138555+DEREA@users.noreply.github.com>
2023-02-27 11:50:04 +0200

    Add files via upload

---
+++ ITEHA Exam Q2/ITEHA Exam Q2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+namespace ITEHA_Exam_Q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating child thread");
+
+            Thread Main = new Thread(StartTimer);
+            Main.Start();
+            
+        }
+
+
+        public static void StartTimer() 
+        {
+            Console.WriteLine("Child thread starts");
+            Console.WriteLine("Child Thread opaused for 5 seconds");
+
+            Thread.Sleep(5000);
+
+            Console.WriteLine("Child thread resumes");
+
+        }
+
+
+    }
+}
