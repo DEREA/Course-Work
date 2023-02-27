26620f869ee3301e03ede755cb61f982b3382d1e
DEREA <89138555+DEREA@users.noreply.github.com>
2023-02-27 12:23:36 +0200

    Add files via upload

---
+++ Test1/Test1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.IO.IsolatedStorage;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me an amount of monkeys");
+            int Monkeys = Convert.ToInt32(Console.ReadLine());
+            int x = 1;
+            
+            while (x != Monkeys)
+            {
+
+                if (x == 1)
+                {
+                    Console.WriteLine(x + " Monkey");
+                }
+
+                else if (x > 1)
+                {
+                    Console.WriteLine(x + " Monkeys");
+                }
+                System.Threading.Thread.Sleep(500);
+                x++;
+            }
+
+            if (Monkeys >= 10) {
+                Console.WriteLine("You have " + Monkeys + " Monkeys. That's a lot of monkeys");
+            }
+            else  {
+
+                Console.WriteLine("You have " + Monkeys + " Monkeys. That's not so many monkeys");
+
+            }
+
+            
+            Console.WriteLine("------------------------------- ");
+
+            Console.WriteLine("How Many Students are there?");
+            int numOfStudents = Convert.ToInt32(Console.ReadLine());
+
+
+            String[] Students = new string[numOfStudents];
+
+            for (int i = 0; i < numOfStudents; i++) {
+                Console.WriteLine("What is the name of student "+(i+1));
+                Students[i] = Console.ReadLine();
+
+
+            }
+
+            Console.WriteLine("------------------------------- ");
+            Console.WriteLine("Listing all students in array ");
+
+            for (int i=0; i<Students.Length; i++) {
+                Console.WriteLine(Students[i]);
+            }
+
+
+
+
+
+        }
+        }
+    }
+
